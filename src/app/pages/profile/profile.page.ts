82e70467a23f5b30b80d7d82c8fac7e9933e42b7
xXTime-OnXx <timon.schmid@bluewin.ch>
2019-11-27 08:59:30 +0100

    upload button

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
-import {Code} from '../../types/code.type';
-import {User} from '../../types/user.type';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.page.html',
-  styleUrls: ['./profile.page.scss'],
+    selector: 'app-profile',
+    templateUrl: './profile.page.html',
+    styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
